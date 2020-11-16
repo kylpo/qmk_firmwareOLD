875609e78a0f4a32d69b3e7759b3216b00285f19
kylpo <2kylepoole@gmail.com>
2020-11-15 20:04:24 -0700

    WIP with LAPROMs

---
+++ keyboards/rhymestone/keymaps/laprom/keymap.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "quantum.h"
+
+// send SEND_STRING() macros depending on whether shift is held
+#define ALT_SHIFT(alt_string_macro, shift_string_macro, shifted) \
+    if (record->event.pressed) {                                 \
+        if (is_shift_down) {                                     \
+            shifted = true;                                      \
+            unregister_code(KC_LSHIFT);                          \
+            shift_string_macro;                                  \
+        } else {                                                 \
+            alt_string_macro;                                    \
+        }                                                        \
+    } else {                                                     \
+        if (shifted && is_shift_down) {                          \
+            register_code(KC_LSHIFT);                            \
+        }                                                        \
+    }                                                            \
+    return false;
+
+// Different keycode when Ctrl is pressed
+// Inspired by https://github.com/qmk/qmk_firmware/tree/master/users/spacebarracecar
+#define WHEN_CTRL(kc, shiftedKc)          \
+    if (record->event.pressed) {          \
+        if (is_ctl_down) {                \
+            if (is_shift_down) {          \
+                register_code(shiftedKc); \
+            } else {                      \
+                register_code(kc);        \
+            }                             \
+        } else {                          \
+            register_code(keycode);       \
+        }                                 \
+    } else {                              \
+        unregister_code(shiftedKc);       \
+        unregister_code(kc);              \
+        unregister_code(keycode);         \
+    }                                     \
+    return false;
+
+#define ENABLE_MOUSE()                          \
+    if (!host_keyboard_led_state().caps_lock) { \
+        tap_code(KC_CAPS);                      \
+    }
+
+#define DISABLE_MOUSE()                        \
+    if (host_keyboard_led_state().caps_lock) { \
+        tap_code(KC_CAPS);                     \
+    }
+
+#define REENABLE_MOUSE()                       \
+    if (host_keyboard_led_state().caps_lock) { \
+        layer_on(_MOUSE);                      \
+    }
---
+++ keyboards/rhymestone/keymaps/laprom30/config.h
@@ -0,0 +1,55 @@
+/* Copyright 2020 marksard
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *l
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define TAPPING_TERM 175
+#define PREVENT_STUCK_MODIFIERS
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// #define TAPPING_TOGGLE 1
+
+// https://github.com/qmk/qmk_firmware/blob/master/quantum/process_keycode/process_combo.h
+#define COMBO_COUNT 3
+#define COMBO_ALLOW_ACTION_KEYS
+// #define COMBO_TERM 40  // try 25?
+
+/* Mouse stuff */
+/* from https://www.reddit.com/r/olkb/comments/72u8ou/qmk_mouse_keys_rock/ */
+// #define MOUSEKEY_INTERVAL 16
+// #define MOUSEKEY_DELAY 0
+// #define MOUSEKEY_TIME_TO_MAX 30
+// #define MOUSEKEY_MAX_SPEED 4
+// #define MOUSEKEY_WHEEL_DELAY 0
+
+// Constant speeds
+#define MK_3_SPEED
+// Hold accel to use it
+#define MK_MOMENTARY_ACCEL
+
+#define MK_C_OFFSET_UNMOD 4     // cursor offset per movement (default: 16)
+#define MK_C_INTERVAL_UNMOD 16  // time between cursor movements (default: 16)
+#define MK_C_OFFSET_2 20        // (default: 32)
+
+// #define MK_COMBINED
+
+// #define MOUSEKEY_MOVE_MAX 40
+
+// If you use the HashTwenty(alpha), please enable USE_HASHTWENTY
+// #define USE_HASHTWENTY
+
+// If you plug in the USB on the right side, please enable MASTER_RIGHT
+// #define MASTER_RIGHT
